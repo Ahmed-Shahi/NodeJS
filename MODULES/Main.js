01da5174a5764941813532d806af394848a82a23
Ahmed-Shahi <ahmedshahi038@gmail.com>
2025-01-19 21:47:38 +0500

    Done!
    Understand the mechanism of Module(import, export)

---
+++ MODULES/Main.js
@@ -0,0 +1,5 @@
+const math = require('./Math')
+
+console.log("Ahmed")
+console.log(math.Age);
+console.log(math.sub(40))
---
+++ MODULES/Math.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a+b
+}
+
+const Age = 20
+
+function sub(a){
+    return a-Age
+}
+
+module.exports = {
+    add,
+    sub,
+    Age
+}
