25e138109ad971605c80ea0d73df1f750a43e801
Ahmed-Shahi <ahmedshahi038@gmail.com>
2025-01-23 15:09:46 +0500

    Done!
    - Create Server
    - Handling URL
    - HTTP Method

--- HTTP SERVER/index.js
+++ HTTP SERVER/index.js
@@ -1,27 +1,36 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req , res)=>{    
    
-    const detail = `${Date.now()}: ${req.url} new request found \n`
-   
-    fs.appendFile("./text.txt", detail, (err )=>{
+    if(req.url === "/favicon.ico") return res.end()
+    
+    const detail = `${Date.now()}: new request found ${req.method} \n`
+    const myUrl =  url.parse(req.url,true)
+    console.log(myUrl);
+    const pathQuery = `Path: ${myUrl.pathname}\nQuery 1: ${myUrl.query.id}\nQuery 2: ${myUrl.query.name}\n`
+    console.log(pathQuery);
+    
+        
+
+    fs.appendFile("./text.txt", detail+pathQuery, (err )=>{
         if(err){
             console.log(err);
         }
         else{
-            res.end("All Good")
+            res.end(`All Good, ${myUrl.query.name}`)
         }
     })
 
-    fs.readFile("./text.txt",'utf-8',(err,data)=>{
-        if(err){
-           console.log("Error"+ err);
-        }
-        else{
-            console.log(data);    
-        }
-    })
+    // fs.readFile("./text.txt",'utf-8',(err,data)=>{
+    //     if(err){
+    //        console.log("Error"+ err);
+    //     }
+    //     else{
+    //         console.log(data);    
+    //     }
+    // })
 
 })
 
